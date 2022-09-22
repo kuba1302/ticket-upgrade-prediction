b4351a70e60ee3b21c226a279e52ae4ea5676c26
Jakub Żmujdzin <jzmujdzin@gmail.com>
2022-09-22 12:30:09 +0200

    bring back old data_prep.py to archive

--- archive/data_prep.py
+++ archive/data_prep.py
@@ -1,330 +1,150 @@
-import os
-from datetime import datetime
-from pathlib import Path
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from loguru import logger
 
 
-class Pipeline:
-    def __init__(
-        self,
-        data_path: str = Path(__file__) / ".." / "WEC2022_Data",
-        data_type: str = "train",
-        model_type: str = "predict_upgrade",
-    ):
-        self.data_path = data_path
-        self.data_type = data_type
-        self.model_type = model_type
-        self.df = self.merge_files()
-        self.concat_df_with_oh_encoding()
+def read_all_csv(data_path, data_type='train', model_type='predict_upgrade'):
+    bkg_drop_cols = ['UPGRADED_FLAG', 'UPGRADE_TYPE', 'UPGRADE_SALES_DATE']
+    bkg = pd.read_csv(data_path / f'BKG_{data_type}.csv', sep=';')
+    if data_type == 'train': 
+        bkg = bkg.drop(columns=bkg_drop_cols)
+    tkt = pd.read_csv(data_path / f'TKT_{data_type}.csv', sep=';')
+    fcp = pd.read_csv(data_path / f'FCP_{data_type}.csv', sep=';')
+    if model_type == 'predict_when_upgrade': 
+        fcp = fcp[fcp['UPGRADED_FLAG'] == 'Y']
+    df = fcp.merge(tkt, on='TICKET_NUMBER', how='left')
+    df = df.merge(bkg, on='BOOKING_ID', how='left')
+    return df 
 
-    def read_csv_file(self, file_prefix) -> pd.DataFrame:
-        return pd.read_csv(
-            self.data_path / f"{file_prefix}_{self.data_type}.csv", sep=";"
+def clean_df(data, data_path, data_type='train', model_type='predict_upgrade'):
+    drop_cols = [
+        "TICKET_NUMBER",
+        "ORIGIN_AIRPORT_CODE",
+        "DESTINATION_AIRPORT_CODE",
+        "SALES_DATE",
+        "FLIGHT_DATE_LOCAL",
+        "MARKETING_CARRIER",
+        "OPERATIONAL_CARRIER",
+        "BOOKED_CLASS",
+        "AIRCRAFT_TYPE",
+        "FARE_BASIS",
+        "BOOKING_ID",
+        "ORIGINAL_TICKET_NUMBER",
+        "SEGMENTS",
+        "FLIGHT_COUPONS",
+        "FORM_OF_PAYMENT",
+        "CURRENCY",
+        "TOTAL_PRICE",
+        "LOYAL_CUSTOMER_ID",
+        "LOYAL_CUSTOMER_DATE_OF_BIRTH",
+        "LOYAL_CUSTOMER_REGISTERED_DATE",
+        "SALES_DATE",
+        "SALES_MARKET",
+        "SEGMENTS",
+        "INTINERARY",
+        "BOOKING_ORIGIN_AIRPORT",
+        "BOOKING_ORIGIN_COUNTRY_CODE",
+        "BOOKING_DEPARTURE_TIME_UTC",
+        "BOOKING_DESTINATION_AIRPORT",
+        "BOOKING_DESTINATION_COUNTRY_CODE",
+        "BOOKING_ARRIVAL_TIME_UTC",
+    ]
+        
+    train_drop_cols = ["UPGRADE_TYPE"]
+    df = data.copy()
+    if model_type == 'predict_upgrade': 
+        train_drop_cols.append("UPGRADE_SALES_DATE")
+    elif model_type == 'predict_when_upgrade': 
+        train_drop_cols.append("UPGRADED_FLAG")
+        df["UPGRADE_SALES_DATE"] = pd.to_datetime(df["UPGRADE_SALES_DATE"])
+        df["BOOKING_DEPARTURE_TIME_UTC"] = pd.to_datetime(df["BOOKING_DEPARTURE_TIME_UTC"])
+
+        df["purchase_time_diff"] = df["UPGRADE_SALES_DATE"] - df["BOOKING_DEPARTURE_TIME_UTC"]
+        df["purchase_time_diff"] = df["purchase_time_diff"].apply(lambda x: x.days)
+        drop_cols.append('UPGRADE_SALES_DATE')
+
+    sus_air_type = ["763", "788", "789", "332", "787"]
+    sus_currency = ["JPY", "USD", "CAD", "SGD", "VND", "AED"]
+    sus_payment = [
+        "UNION",
+        "CCDS6",
+        "CCSW9",
+        "NET R",
+        "CCJC3",
+        "CCAX3",
+        "BARTE",
+        "CCVI4",
+        "PAY24",
+    ]
+    df = df[df['TOTAL_PRICE_PLN'] > 0]
+    df = df[df['BOOKING_WINDOW_D'] != -1]
+    df = df[df['FLIGHT_DISTANCE'] >= 0]
+    df["FLIGHT_DATE_LOCAL"] = pd.to_datetime(df["FLIGHT_DATE_LOCAL"])
+    df["SALES_DATE"] = pd.to_datetime(df["SALES_DATE"])
+    df["sale_to_flight_time"] = df["FLIGHT_DATE_LOCAL"] - df["SALES_DATE"]
+    df["sale_to_flight_time"] = df["sale_to_flight_time"].apply(lambda x: x.days)
+
+    stay_lenght_map = {-9999: np.nan}
+    df["STAY_LENGTH_D"] = df["STAY_LENGTH_D"].replace(stay_lenght_map)
+    df["BOOKING_ARRIVAL_TIME_UTC"] = pd.to_datetime(df["BOOKING_ARRIVAL_TIME_UTC"])
+    df["BOOKING_DEPARTURE_TIME_UTC"] = pd.to_datetime(df["BOOKING_DEPARTURE_TIME_UTC"])
+
+    df["flight_len"] = df["BOOKING_ARRIVAL_TIME_UTC"] - df["BOOKING_DEPARTURE_TIME_UTC"]
+    df["flight_len"] = df["flight_len"].apply(lambda x: x.seconds / 3600)
+
+    df["TIME_DEPARTURE_LOCAL_TIME"] = pd.to_datetime(df["TIME_DEPARTURE_LOCAL_TIME"])
+    df["TIME_DEPARTURE_LOCAL_TIME"] = df["TIME_DEPARTURE_LOCAL_TIME"].apply(
+        lambda x: x.hour
+    )
+
+    def get_if_add_upgrade(df, data_type):
+        emd = pd.read_csv(data_path / f"EMD_{data_type}.csv", sep=";")
+        return np.where(
+            np.isin(df["TICKET_NUMBER"], emd["REFERENCE_TICKET_NUMBER"].unique()), 1, 0
         )
 
-    def read_all_files(self) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-        bkg = (
-            self.read_csv_file("BKG").drop(columns=self.get_bkg_drop_cols())
-            if self.data_type == "train"
-            else self.read_csv_file("BKG")
-        )
-        logger.info("loaded booking data")
-        tkt = self.read_csv_file("TKT")
-        logger.info("loaded ticket data")
-        fcp = self.read_csv_file("FCP")
-        logger.info("loaded flight coupon data")
-        return (
-            bkg,
-            tkt,
-            fcp[fcp.UPGRADED_FLAG == "Y"]
-            if self.model_type == "predict_when_upgrade"
-            else fcp,
-        )
+    df["if_additional_upgrade"] = get_if_add_upgrade(df, data_type=data_type)
+    df["same_carrier"] = np.where(
+        df["MARKETING_CARRIER"] == df["OPERATIONAL_CARRIER"], 1, 0
+    )
+    df["is_sus_aircraft"] = np.where(np.isin(df["AIRCRAFT_TYPE"], sus_air_type), 1, 0)
 
-    def merge_files(self) -> pd.DataFrame:
-        bkg, tkt, fcp = self.read_all_files()
-        logger.info(f"{datetime.now()} merging datasets")
-        return fcp.merge(tkt, on="TICKET_NUMBER", how="left").merge(
-            bkg, on="BOOKING_ID", how="left"
-        )
+    if data_type == 'train': 
+        df["UPGRADED_FLAG"] = df["UPGRADED_FLAG"].map({"Y": 1, "N": 0})
 
-    @staticmethod
-    def discard_nonupgraded_rows(fcp: pd.DataFrame) -> pd.DataFrame:
-        return fcp[fcp["UPGRADED_FLAG"] == "Y"]
+    df["is_sus_payment"] = np.where(np.isin(df["FORM_OF_PAYMENT"], sus_payment), 1, 0)
+    df["intinerary_len"] = df["INTINERARY"].apply(lambda x: len(x.split("-")))
+    df["is_sus_currency"] = np.where(np.isin(df["FORM_OF_PAYMENT"], sus_currency), 1, 0)
 
-    @staticmethod
-    def get_bkg_drop_cols() -> list:
-        return ["UPGRADED_FLAG", "UPGRADE_TYPE", "UPGRADE_SALES_DATE"]
+    df["PAX_GENDER"] = df["PAX_GENDER"].map({"M": 1, "F": 0})
 
-    @staticmethod
-    def get_columns_to_drop() -> list:
-        return [
-            "TICKET_NUMBER",
-            "ORIGIN_AIRPORT_CODE",
-            "DESTINATION_AIRPORT_CODE",
-            "SALES_DATE",
-            "FLIGHT_DATE_LOCAL",
-            "MARKETING_CARRIER",
-            "OPERATIONAL_CARRIER",
-            "BOOKED_CLASS",
-            "AIRCRAFT_TYPE",
-            "FARE_BASIS",
-            "BOOKING_ID",
-            "ORIGINAL_TICKET_NUMBER",
-            "SEGMENTS",
-            "FLIGHT_COUPONS",
-            "FORM_OF_PAYMENT",
-            "CURRENCY",
-            "TOTAL_PRICE",
-            "LOYAL_CUSTOMER_ID",
-            "LOYAL_CUSTOMER_DATE_OF_BIRTH",
-            "LOYAL_CUSTOMER_REGISTERED_DATE",
-            "SALES_DATE",
-            "SALES_MARKET",
-            "SEGMENTS",
-            "INTINERARY",
-            "BOOKING_ORIGIN_AIRPORT",
-            "BOOKING_ORIGIN_COUNTRY_CODE",
-            "BOOKING_DEPARTURE_TIME_UTC",
-            "BOOKING_DESTINATION_AIRPORT",
-            "BOOKING_DESTINATION_COUNTRY_CODE",
-            "BOOKING_ARRIVAL_TIME_UTC",
-        ]
+    df["CORPORATE_CONTRACT_FLG"] = df["CORPORATE_CONTRACT_FLG"].map({"Y": 1, "N": 0})
+    df["LOYAL_CUSTOMER"] = df["LOYAL_CUSTOMER"].map({"Y": 1, "N": 0})
 
-    def get_cols_to_drop_for_training(self) -> list:
-        cols = ["UPGRADE_TYPE"]
-        cols.append(
-            "UPGRADE_SALES_DATE"
-        ) if self.model_type == "predict_upgrade" else cols.append("UPGRADED_FLAG")
-        return cols
+    df["BOOKING_LONG_HOUL_FLAG"] = df["BOOKING_LONG_HOUL_FLAG"].map({"Y": 1, "N": 0})
+    df["BOOKING_DOMESTIC_FLAG"] = df["BOOKING_DOMESTIC_FLAG"].map({"Y": 1, "N": 0})
 
-    def prepare_new_cols_for_predict_when_model(self) -> None:
-        self.df["purchase_time_diff"] = (
-            self.df["UPGRADE_SALES_DATE"] - self.df["BOOKING_DEPARTURE_TIME_UTC"]
-        )
-        self.df["purchase_time_diff"] = self.df["purchase_time_diff"].apply(
-            lambda x: x.days
-        )
-        logger.info('prepared new cols for predict when model')
+    if data_type == 'train': 
+        df = df.drop(columns=train_drop_cols)
 
-    @staticmethod
-    def get_sus_air_type() -> list:
-        return ["763", "788", "789", "332", "787"]
-
-    @staticmethod
-    def get_sus_currency() -> list:
-        return ["JPY", "USD", "CAD", "SGD", "VND", "AED"]
-
-    @staticmethod
-    def get_sus_payment() -> list:
-        return [
-            "UNION",
-            "CCDS6",
-            "CCSW9",
-            "NET R",
-            "CCJC3",
-            "CCAX3",
-            "BARTE",
-            "CCVI4",
-            "PAY24",
-        ]
-
-    def filter_wrong_ticket_prices(self) -> None:
-        self.df = self.df[self.df["TOTAL_PRICE_PLN"] > 0]
-        logger.info("filtered wrong ticket prices")
-
-    @staticmethod
-    def get_datetime_columns() -> list:
-        return [
-            "UPGRADE_SALES_DATE",
-            "BOOKING_DEPARTURE_TIME_UTC",
-            "FLIGHT_DATE_LOCAL",
-            "SALES_DATE",
-            "BOOKING_ARRIVAL_TIME_UTC",
-            "TIME_DEPARTURE_LOCAL_TIME",
-        ]
-
-    def convert_datetime_columns_to_pandas_format(self) -> None:
-        self.df[self.get_datetime_columns()] = self.df[
-            self.get_datetime_columns()
-        ].apply(pd.to_datetime)
-        logger.info("converted datetime columns to pandas format")
-
-    def filter_wrong_booking_window(self) -> None:
-        self.df = self.df[self.df["BOOKING_WINDOW_D"] != -1]
-        logger.info("filtered wrong booking windows")
-
-    def filter_nonpositive_flight_distance(self) -> None:
-        self.df = self.df[self.df["FLIGHT_DISTANCE"] >= 0]
-        logger.info("filtered nonpositive flight distance")
-
-    def get_sale_to_flight_time(self) -> None:
-        self.df["sale_to_flight_time"] = (
-            self.df["FLIGHT_DATE_LOCAL"] - self.df["SALES_DATE"]
-        )
-        self.df["sale_to_flight_time"] = self.df["sale_to_flight_time"].apply(
-            lambda x: x.days
-        )
-        logger.info("got sale to flight time col")
-
-    @staticmethod
-    def get_stay_length_map() -> dict:
-        return {-9999: np.nan}
-
-    def map_stay_length(self) -> None:
-        self.df["STAY_LENGTH_D"] = self.df["STAY_LENGTH_D"].replace(
-            self.get_stay_length_map()
-        )
-
-    def get_flight_len(self) -> None:
-        self.df["flight_len"] = (
-            self.df["BOOKING_ARRIVAL_TIME_UTC"] - self.df["BOOKING_DEPARTURE_TIME_UTC"]
-        )
-        self.df["flight_len"] = self.df["flight_len"].apply(lambda x: x.seconds / 3600)
-        logger.info("got flight len col")
-
-    def map_departure_time_to_hours(self) -> None:
-        self.df["TIME_DEPARTURE_LOCAL_TIME"] = self.df[
-            "TIME_DEPARTURE_LOCAL_TIME"
-        ].apply(lambda x: x.hour)
-        logger.info("mapped departure time to hours")
-
-    @staticmethod
-    def get_yes_no_binary_map() -> dict:
-        return {"Y": 1, "N": 0}
-
-    def check_for_add_upgrade(self) -> None:
-        emd = self.read_csv_file("EMD")
-        self.df["if_additional_upgrade"] = np.where(
-            np.isin(self.df["TICKET_NUMBER"], emd["REFERENCE_TICKET_NUMBER"].unique()),
-            1,
-            0,
-        )
-        logger.info("checked for add upgrade")
-
-    def check_for_same_carrier(self) -> None:
-        self.df["same_carrier"] = np.where(
-            self.df["MARKETING_CARRIER"] == self.df["OPERATIONAL_CARRIER"], 1, 0
-        )
-        logger.info("checked for same carrier")
-
-    def check_for_sus_aircraft(self) -> None:
-        self.df["is_sus_aircraft"] = np.where(
-            np.isin(self.df["AIRCRAFT_TYPE"], self.get_sus_air_type()), 1, 0
-        )
-        logger.info("checked for sus aircraft")
-
-    def map_target_variable_for_training(self) -> None:
-        self.df["UPGRADED_FLAG"] = self.df["UPGRADED_FLAG"].map(
-            self.get_yes_no_binary_map()
-        )
-
-    def check_for_sus_payment(self) -> None:
-        self.df["is_sus_payment"] = np.where(
-            np.isin(self.df["FORM_OF_PAYMENT"], self.get_sus_payment()), 1, 0
-        )
-        logger.info("checked for sus payment")
-
-    def get_intinerary_len(self) -> None:
-        self.df["intinerary_len"] = self.df["INTINERARY"].apply(
-            lambda x: len(x.split("-"))
-        )
-        logger.info("got itinerary len col")
-
-    def check_for_sus_currency(self) -> None:
-        self.df["is_sus_currency"] = np.where(
-            np.isin(self.df["FORM_OF_PAYMENT"], self.get_sus_currency()), 1, 0
-        )
-        logger.info("checked for sus currency")
-
-    @staticmethod
-    def get_map_gender() -> dict:
-        return {"M": 1, "F": 0}
-
-    def map_genders(self) -> None:
-        self.df["PAX_GENDER"] = self.df["PAX_GENDER"].map(self.get_map_gender())
-        logger.info("mapped genders")
-
-    def map_corporate_contract_flg(self) -> None:
-        self.df["CORPORATE_CONTRACT_FLG"] = self.df["CORPORATE_CONTRACT_FLG"].map(
-            self.get_yes_no_binary_map()
-        )
-        logger.info("mapped corporate contract flag")
-
-    def map_loyal_customer(self) -> None:
-        self.df["LOYAL_CUSTOMER"] = self.df["LOYAL_CUSTOMER"].map(
-            self.get_yes_no_binary_map()
-        )
-        logger.info("mapped loyal customers")
-
-    def map_booking_long_houl_flag(self) -> None:
-        self.df["BOOKING_LONG_HOUL_FLAG"] = self.df["BOOKING_LONG_HOUL_FLAG"].map(
-            self.get_yes_no_binary_map()
-        )
-        logger.info("mapped booking long houl flag")
-
-    def map_booking_domestic_flag(self) -> None:
-        self.df["BOOKING_DOMESTIC_FLAG"] = self.df["BOOKING_DOMESTIC_FLAG"].map(
-            self.get_yes_no_binary_map()
-        )
-        logger.info("mapped booking domestic flag")
-
-    def clean_df(self):
-        logger.info(f"{datetime.now()} cleaning df ...")
-        self.convert_datetime_columns_to_pandas_format()
-        self.prepare_new_cols_for_predict_when_model() if self.model_type == 'predict_when_upgrade' else None
-        self.filter_wrong_ticket_prices()
-        self.filter_wrong_booking_window()
-        self.filter_nonpositive_flight_distance()
-        self.get_sale_to_flight_time()
-        self.get_flight_len()
-        self.map_departure_time_to_hours()
-        self.check_for_add_upgrade()
-        self.check_for_same_carrier()
-        self.check_for_sus_aircraft()
-        self.map_target_variable_for_training() if self.data_type == "train" else None
-        self.check_for_sus_payment()
-        self.get_intinerary_len()
-        self.check_for_sus_currency()
-        self.map_genders()
-        self.map_corporate_contract_flg()
-        self.map_loyal_customer()
-        self.map_booking_long_houl_flag()
-        self.map_booking_domestic_flag()
-        self.df = (
-            self.df.drop(columns=self.get_cols_to_drop_for_training())
-            if self.data_type == "train"
-            else self.df
-        )
-        self.df.drop(columns=self.get_columns_to_drop(), inplace=True)
-        logger.info(f"dropped unnecessary columns. finished cleaning df")
-
-    @staticmethod
-    def get_oh_cols() -> list:
-        return [
-            "FLIGHT_RANGE",
-            "BOOKED_CABIN",
-            "VAB",
-            "PAX_TYPE",
-            "SALES_CHANNEL",
-            "TRIP_TYPE",
-        ]
-
-    def get_oh_encoding(self) -> pd.DataFrame:
-        logger.info(f"{datetime.now()} getting OH encoding")
-        return pd.get_dummies(
-            self.df[self.get_oh_cols()], prefix=self.get_oh_cols(), drop_first=True
-        )
-
-    def concat_df_with_oh_encoding(self) -> None:
-        self.clean_df()
-        self.df = pd.concat([self.df, self.get_oh_encoding()], axis=1).drop(
-            columns=self.get_oh_cols()
-        )
+    return df.drop(columns=drop_cols)
 
 
-if __name__ == "__main__":
-    d = Pipeline(model_type='predict_when_upgrade').df
+def oh_encoding(data): 
+    df = data.copy()
+    oh_cols = [
+        "FLIGHT_RANGE",
+        "BOOKED_CABIN",
+        "VAB",
+        "PAX_TYPE",
+        "SALES_CHANNEL",
+        "TRIP_TYPE",
+    ]
+    oh_df = pd.get_dummies(data[oh_cols], prefix=oh_cols, drop_first=True)
+    return pd.concat([df, oh_df], axis=1).drop(columns=oh_cols)
+
+
+def data_pipeline(data_path, model_type, data_type): 
+    df = read_all_csv(data_path=data_path, data_type=data_type, model_type=model_type)
+    clean_data = clean_df(data=df, data_path=data_path, data_type=data_type, model_type=model_type)
+    return oh_encoding(clean_data)
