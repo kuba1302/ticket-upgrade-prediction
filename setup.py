89affc2484ae819c4feca91f68132ab4e83c447a
kuba1302 <kuba1302@gmail.com>
2022-09-23 20:04:27 +0200

    fix workflows, add init, rename pipeline

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="industres-needs-detection",
-    version="0.1",
-    author="Jakub Wujec, Jakub Zmujdzin, Hubert Wojewoda, Adam Janczyszyn",
-    packages=find_packages(),
-    install_requires=[],
-)
