e00ba931d52673a095a3131ff35c80075f9c46f3
kuba1302 <kuba1302@gmail.com>
2022-10-13 00:26:22 +0200

    add model trainer

--- ticket_upgrade_prediction/models/neural_net_torch/train.py
+++ ticket_upgrade_prediction/models/neural_net_torch/train.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
 from pathlib import Path
+from typing import Any
 
 import click
 import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
 import torch
 from data_loader import UpgradeDataset
 from loguru import logger
@@ -12,14 +14,103 @@ from torch.nn import BCEWithLogitsLoss
 from torch.optim import SGD, Adam
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-
+from ticket_upgrade_prediction.pipeline import Dataset
 from ticket_upgrade_prediction import Evaluator, Metrics
+from sklearn.model_selection import train_test_split
 
 
-@dataclass
-class Results:
-    epoch: int
-    metrics: Metrics
+class ModelTrainer:
+    def __init__(
+        self,
+        dataset: Dataset,
+        layers: list = [5],
+        optimizer_name: str = "Adam",
+        epochs: int = 2,
+        learning_rate: int = 0.0001,
+        batch_size: int = 64,
+        train_size: float = 0.75,
+        criterion: Any = BCEWithLogitsLoss,
+    ) -> None:
+        self.layers = layers
+        self.model = Network(
+            input_size=dataset.X_train.shape[1], hidden_layers_sizes=layers
+        )
+        self.optimizer = self._get_optimizer(
+            optimizer_name=optimizer_name, learning_rate=learning_rate
+        )
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self.epochs = epochs
+        self.criterion = criterion
+        self.batch_size = batch_size
+        self.train_size = train_size
+
+        self.training_results = []
+
+    def _get_optimizer(
+        self, optimizer_name: str, learning_rate: float
+    ) -> None:
+        if optimizer_name.lower() == "adam":
+            self.optimizer = Adam(self.model.parameters(), lr=learning_rate)
+        elif optimizer_name.lower() == "sgd":
+            self.optimizer = SGD(self.model.parameters(), lr=learning_rate)
+        else:
+            raise ValueError("Wrong type of optimizer provided!")
+
+    def _one_batch(self, X: torch.Tensor, y: torch.Tensor) -> None:
+        X.to(self.device)
+        y.to(self.device)
+
+        preds_logit = self.model(X.float())
+        loss = self.criterion(preds_logit.float(), y.float())
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def fit(self, to_mlflow: bool = False):
+        train_dataset = UpgradeDataset(
+            X=self.dataset.X_train, y=self.dataset.y_train.values
+        )
+        train_loader = DataLoader(
+            dataset=train_dataset, batch_size=self.batch_size, shuffle=True
+        )
+        for epoch in tqdm(range(self.epochs), desc="Epoch"):
+            for X, y in tqdm(iter(train_loader), desc="Batch"):
+                self._one_batch(X=X, y=y)
+                with torch.no_grad():
+                    self._evaluate(to_mlflow=to_mlflow, epoch=epoch)
+
+    def _evaluate(self, to_mlflow: bool, epoch: int) -> None:
+        evaluator = Evaluator(
+            model=self.model, X=self.dataset.X_test, y=self.dataset.y_test
+        )
+        metrics = evaluator.get_all_metrics(epoch=epoch, to_mlflow=to_mlflow)
+        logger.info(metrics)
+        self.training_results.append(metrics)
+
+    def get_results(self) -> list[Metrics]:
+        return self.training_results
+
+
+class KFoldModelTrainer:
+    def __init__(
+        self, data: pd.DataFrame, n_splits: int, scaler: Any = StandardScaler
+    ) -> None:
+        self.n_splits = n_splits
+        self.data = data
+        self.scaler = scaler
+        self.metrics = []
+
+    def _one_batch_train(self):
+        pass
+
+    def _train_test_split(self):
+        X_train, X_test, y_train, y_test = train_test_split()
+
+    def hyperopt(self, target_metric):
+        pass
 
 
 def train_model(
@@ -76,11 +167,10 @@ def train_model(
 
             with torch.no_grad():
                 evaluator = Evaluator(model=model, X=X_test, y=y_test)
-                metrics = evaluator.get_all_metrics()
+                metrics = evaluator.get_all_metrics(epoch=epoch)
                 logger.info(metrics)
 
-                result = Results(epoch=epoch, metrics=metrics)
-                training_results.append(result)
+                training_results.append(metrics)
 
 
 @click.command()
