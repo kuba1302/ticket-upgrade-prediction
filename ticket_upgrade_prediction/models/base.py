c577b3ce5276ec8623133c7f0ca5873e1ca9efe9
kuba1302 <kuba1302@gmail.com>
2022-10-28 00:49:06 +0200

    add sklearn type

--- ticket_upgrade_prediction/models/base.py
+++ ticket_upgrade_prediction/models/base.py
@@ -1,4 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import Union
+import numpy as np
+
+from xgboost import XGBClassifier, XGBRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+# !TODO add lightgbm
+SklearnClassifier = Union[
+    LogisticRegression,
+    KNeighborsClassifier,
+    RandomForestClassifier,
+    XGBClassifier,
+]
+SklearnRegressor = Union[
+    LinearRegression,
+    KNeighborsRegressor,
+    RandomForestRegressor,
+    XGBRegressor,
+]
+
+SklearnModel = Union[SklearnClassifier, SklearnRegressor]
 
 
 class BaseModel(ABC):
@@ -11,15 +34,15 @@ class BaseModel(ABC):
     """
 
     @abstractmethod
-    def predict(self, X):
+    def predict(self, X) -> np.ndarray:
         """Sklearn-like predict function"""
 
     @abstractmethod
-    def predict_proba(self, X):
+    def predict_proba(self, X) -> np.ndarray:
         """Sklearn-like predict probability function"""
 
     @abstractmethod
-    def fit_model(self, **kwargs):
+    def fit_model(self, **kwargs) -> None:
         """Fit model on new data"""
 
     @abstractmethod
@@ -27,9 +50,11 @@ class BaseModel(ABC):
         """Retrieve fitted model from MLflow"""
 
     @abstractmethod
-    def save_model_to_pickle(self, model_name):
+    def save_model_to_pickle(self, model_name) -> None:
         """Save model to .pkl"""
 
     @abstractmethod
-    def save_model_to_mlflow(self, model_name, artifact_path, X_test, y_test):
+    def save_model_to_mlflow(
+        self, model_name, artifact_path, X_test, y_test
+    ) -> None:
         """Save model to mlflow"""
